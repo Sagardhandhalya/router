4631499ff31e24528527f189b648531f057c4d0c
dhandhalyasagar0@gmail.com <dhandhalyasagar0@gmail.com>
2019-03-10 13:36:53 +0530

    Updates

---
+++ precache-manifest.78380147be64706ed5a1c411bf037574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "8cb8b3f990952ee6f777",
+    "url": "/static/js/main.1b88b39d.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "8cb8b3f990952ee6f777",
+    "url": "/static/css/main.a580e5e0.chunk.css"
+  },
+  {
+    "revision": "a761322d2593ae5c9b307d8ac09c8e02",
+    "url": "/index.html"
+  }
+];
